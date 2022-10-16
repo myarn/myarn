3acb9dc6bab5a5c808a9f8a73ce8d696b3f92973
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2022-10-16 23:20:42 +0900

    Semicolon;

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,16 +1,16 @@
-export const clientName = 'MyarnMC Client (https://github.com/myarnmc)'
+export const userAgent = 'MyarnMC Client (https://github.com/myarn)'
 
 export const request = async (...args: Parameters<typeof fetch>) => {
   const req: RequestInit = args[1] || {};
   if (!req.headers) req.headers = new Headers();
   
-  if (req.headers instanceof Headers) req.headers.set('User-Agent', clientName);
-  else if (Array.isArray(req.headers)) req.headers.push(['User-Agent', clientName]);
-  else req.headers['User-Agent'] = clientName;
+  if (req.headers instanceof Headers) req.headers.set('User-Agent', userAgent);
+  else if (Array.isArray(req.headers)) req.headers.push(['User-Agent', userAgent]);
+  else req.headers['User-Agent'] = userAgent;
 
   const response = await fetch(args[0], req);
 
   if (!response.ok) throw new Error(`Request to ${args[0]} failed.`);
 
   return response;
-}
+};
