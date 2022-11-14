d5300dd9fce8008a835760a838808fe472319f95
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2022-11-14 19:45:19 +0900

    Update deps.ts

--- src/deps.ts
+++ src/deps.ts
@@ -3,5 +3,12 @@ export * from "https://deno.land/std@0.158.0/hash/mod.ts";
 export * from "https://deno.land/std@0.158.0/path/mod.ts";
 
 // Third Party
-export * from 'https://deno.land/x/cliffy@v0.25.2/command/mod.ts';
+export * from 'https://deno.land/x/cliffy@v0.25.4/command/mod.ts';
+export * from 'https://deno.land/x/cliffy@v0.25.4/prompt/mod.ts';
+export * from 'https://deno.land/x/cliffy@v0.25.4/ansi/mod.ts';
 export * as YAML from 'https://deno.land/std@0.157.0/encoding/yaml.ts';
+
+import { Octokit as OctokitCore } from 'https://cdn.skypack.dev/@octokit/core?dts';
+import { restEndpointMethods } from 'https://cdn.skypack.dev/@octokit/plugin-rest-endpoint-methods?dts';
+
+export const Octokit = OctokitCore.plugin(restEndpointMethods);
