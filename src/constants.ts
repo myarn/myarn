996274e6b2d6c31df336e45bf081acda3ce97ee0
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2023-05-05 13:46:44 +0900

    Add user agent

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const DEFAULT_CONFIG_FILES = [
   'myarn.yaml'
 ];
+
+export const MYARN_VERSION = '0.0.1';
---
+++ src/core/download/userAgent.ts
@@ -0,0 +1,3 @@
+import { MYARN_VERSION } from '../../constants.ts';
+
+export const USER_AGENT = `myarn/myarn/${MYARN_VERSION}`
